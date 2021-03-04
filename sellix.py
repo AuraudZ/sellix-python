10fed6ad724319a3212c8ad4bb72ed50c60766b2
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-03-04 14:36:56 -0800

    Sellix Python Bot

---
+++ sellix.py
@@ -0,0 +1,86 @@
+import discord
+from discord import channel
+from discord.client import Client
+import dotenv
+import requests
+import json
+import method_handler as mh
+import time
+import os
+import requests
+import os
+from requests.api import head
+from requests.structures import CaseInsensitiveDict
+from os.path import join, dirname
+from dotenv import load_dotenv 
+from os import system
+from discord.ext import commands
+bot = commands.Bot(command_prefix='!')
+bot.remove_command("help")
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+token = os.getenv('TOKEN')
+secret_key = os.getenv('API_KEY')
+
+@bot.event
+async def on_ready():
+    print('-------')
+    print('Online')
+    print('-------')
+
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    embed = discord.Embed(title="-_-", description="You are using that command wrong, please retry with the correct roles or arguments.")
+    embed.set_footer(text="-skeet")
+    await ctx.send(embed=embed)
+    print(error)
+
+
+@bot.command()
+async def list(ctx):
+    #post_headers = {'Authorization' : 'Bearer {SELLIX_API_KEY}}
+    #token="258deGySwb3gUJqCXQKcZ5woIeLm8QbLYLwwUC5g3aIzBr1uUi3VX0wLLq2RqxPC"
+  
+    order = "https://dev.sellix.io/v1/orders"
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    print (secret_key)
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    head= 'Bearer '+secret_key
+    headers["Authorization"] =  head
+    resp = requests.get(order, headers=headers)
+    output = resp.text
+    parsed = json.loads(output)
+    file="all.json"
+    o = open(file,"w")
+    o.write(json.dumps(parsed, indent=2))
+    #print (json.dumps(parsed, indent=2))
+    await ctx.send(file=discord.File(file))
+
+@bot.command()
+async def order(ctx,description):
+    #post_headers = {'Authorization' : 'Bearer {SELLIX_API_KEY}}
+    #token="258deGySwb3gUJqCXQKcZ5woIeLm8QbLYLwwUC5g3aIzBr1uUi3VX0wLLq2RqxPC"
+  
+    order = "https://dev.sellix.io/v1/orders/" + description
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    secret_key = os.getenv('API_KEY')
+    print (secret_key)
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    head= 'Bearer '+secret_key
+    headers["Authorization"] =  head
+    resp = requests.get(order, headers=headers)
+    output = resp.text
+    parsed = json.loads(output)
+    file ="order " + description
+    o = open(file,"w")
+    o.write(json.dumps(parsed, indent=2))
+   # print (json.dumps(parsed, indent=2))
+    print(description)
+    await ctx.send(file=discord.File(file))
+bot.run(token)
